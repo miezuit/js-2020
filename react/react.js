4718274536e5b83799f8d099d2ff588a9db7eb76
miezuit <miezuit@gmail.com>
2021-06-04 19:24:38 +0300

    changes

--- react/react.js
+++ react/react.js
@@ -1,4 +1,36 @@
-const template = <h1>Hello</h1>
+class Welcome extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return <h1>Hello {this.props.name.toUpperCase() } </h1>
+    }
+}
+class Clicker extends React.Component {
+    constructor() {
+        super()
+        // starea initiala a componentei
+        this.state = {
+            clicks: 0,
+        }
+    }
+    // definind metoda ca arrow function se face automat bind la this
+    increment = () => {
+        this.setState({
+            clicks: this.state.clicks + 1
+        })
+    };
+    render() {
+        return <button onClick={this.increment}>You clicked {this.state.clicks} times</button>
+    }
+}
+
+const template = (
+<div>
+    <Welcome name='Radu'></Welcome>
+    <Clicker></Clicker>
+</div>
+)
 
 ReactDOM.render(
     template,
